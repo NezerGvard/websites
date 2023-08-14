b9f52329ba0ef051dc5783683b37d8bdce9df778
NezerGvard <gglol.cyk@gmail.com>
2023-08-14 11:57:54 +0300

    prototype

---
+++ prototype/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./prototype/**/*.{html,js}",
+  
+  ],
+  theme: {
+    colors: require('tailwindcss/colors'),
+    extend: {},
+  },
+  plugins: [],
+}
